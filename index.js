32127cfdbcb5f843c93fdb08cd728e23cb1277a0
Nayan <dreamerboynayan@gmail.com>
2025-04-23 06:20:50 +0600

    Variable Scope

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 // ES6 Variables 
 
-let a = 10;
-const b = 20;
+// let a = 10;
+// const b = 20;
 
-console.log(a + b);
+// console.log(a + b);
+
+// Variable Scope 
+
+
+// Initialize a global variable
+
+const a = 20;
+
+function varScope(){
+// Initialize a block-scoped variable
+
+   let a = 30;
+    console.log("Variable from inside the function : " + a);
+}
+varScope()
+console.log("Variable from outside the function : " + a);
