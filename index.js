3f4f992d28f354314e7564fb32d48e3c30acf691
Nayan <dreamerboynayan@gmail.com>
2025-04-23 06:35:41 +0600

    Hoisting

--- index.js
+++ index.js
@@ -10,13 +10,37 @@
 
 // Initialize a global variable
 
-const a = 20;
+// const a = 20;
 
-function varScope(){
-// Initialize a block-scoped variable
+// function varScope(){
+// // Initialize a block-scoped variable
 
-   let a = 30;
-    console.log("Variable from inside the function : " + a);
+//    let a = 30;
+//     console.log("Variable from inside the function : " + a);
+// }
+// varScope()
+// console.log("Variable from outside the function : " + a);
+
+// Hoisting 
+
+console.log(a);
+
+const a = 20; // ReferenceError
+
+let b = 50;
+console.log(b) // 50
+
+funcHoisting() // 70
+function funcHoisting(){
+    let a = 50;
+    let b = 20;
+    const sum = a + b;
+    console.log(sum)
+} 
+
+func(70,30) // ReferenceError
+const func = (num1, num2) => {
+let result = num1 + num2;
+console.log(result)
 }
-varScope()
-console.log("Variable from outside the function : " + a);
+
